6878418398caece7ab3259baf557d9b2fa0bb58c
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2017-06-05 23:32:11 +0900

    added font

--- app/src/main/java/tomoyaapp/trpgcalculator/MainActivity.java
+++ app/src/main/java/tomoyaapp/trpgcalculator/MainActivity.java
@@ -1,14 +1,21 @@
 package tomoyaapp.trpgcalculator;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-    private TextView titleImageView;
-    private Typeface typeface;
+public class MainActivity extends Activity implements View.OnClickListener{
+    private TextView titleTextView;
+    private TextView mainTextView;
+    private EditText editText;
+    private ImageButton imageButton;
+    private Typeface typeface, typeface2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,21 @@ public class MainActivity extends Activity {
 
 
         typeface = Typeface.createFromAsset(getAssets(), "genkai_mincho.ttf");
+        typeface2 = Typeface.createFromAsset(getAssets(), "aroy.ttf");
 
-        titleImageView = (TextView)findViewById(R.id.titleIv);
+        titleTextView = (TextView)findViewById(R.id.titleIv);
+        mainTextView = (TextView)findViewById(R.id.mainTv);
+        editText = (EditText) findViewById(R.id.editText);
+        imageButton = (ImageButton)findViewById(R.id.btn);
+        imageButton.setOnClickListener(this);
 
-        titleImageView.setTypeface(typeface);
+
+        titleTextView.setTypeface(typeface);
+        mainTextView.setTypeface(typeface2);
+    }
+
+    @Override
+    public void onClick(View view){
+
     }
 }
